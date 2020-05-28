e3a1a7cd67aeac1e91625ce07a095b21bb138dd5
zawibe <beata.zawidlak@gmail.com>
2020-05-28 19:43:30 +0200

    changes added

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,25 +1,61 @@
 package com.company.enroller.persistence;
 
-import com.company.enroller.model.Meeting;
+import java.io.Serializable;
+import java.util.Collection;
+
 import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
+import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 
 @Component("meetingService")
 public class MeetingService {
 
-    DatabaseConnector connector;
+	Session session;
 
-    public MeetingService() {
-        connector = DatabaseConnector.getInstance();
-    }
+	public MeetingService() {
+		session = DatabaseConnector.getInstance().getSession();
+	}
 
-    public Collection<Meeting> getAll() {
-        String hql = "FROM Meeting";
-        Query query = connector.getSession().createQuery(hql);
-        return query.list();
-    }
+	public Collection<Meeting> getAll() {
+		String hql = "FROM Meeting";
+		Query query = session.createQuery(hql);
+		return query.list();
+	}
 
+	public Meeting findById(long id) {
+		return (Meeting) session.get(Meeting.class, id);
+	}
 
-}
+	public Meeting add(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		session.save(meeting);
+		transaction.commit();
+		return meeting;
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.delete(meeting);
+		transaction.commit();
+	}
+
+	public void addParticipant(Participant participant) {
+		Transaction transaction = this.session.beginTransaction();
+		addParticipant(participant);
+		this.session.merge(participant);
+		transaction.commit();
+	}
+
+	public Meeting updateMeeting(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		session.update(meeting);
+		transaction.commit();
+		return meeting;
+	}
+	
+	}
+
